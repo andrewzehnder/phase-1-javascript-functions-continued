9f213e1e88d2759463ddd4f4cb514e3a5d764b1a
Andrew <azehnder2@gmail.com>
2022-04-08 16:58:16 -0700

    commit tree d54a7d554962fd190cc0b56a0d4dd3500a02dc0c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (typeof activity === 'undefined') {
+        activity = "roller-skate"
+    }
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(mondayActivity) {
+    if (typeof mondayActivity === 'undefined') {
+        mondayActivity = "go to the office"
+    }
+    return (`This Monday, I will ${mondayActivity}.`)
+}
+
+function wrapAdjective(highlight) {
+    return function(emphatic = "special") {
+        if (highlight === "*") {
+            emphatic = "a hard worker";
+        }
+        if (highlight === "||") {
+            emphatic = "a dedicated programmer";
+        }
+        return `You are ${highlight}${emphatic}${highlight}!`;
+    }
+}
